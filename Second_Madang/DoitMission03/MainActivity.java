ef9ad3b3a466cc6089d4fdad3ad533e149aaf741
joona <103545438+s-joona@users.noreply.github.com>
2022-10-22 20:06:55 +0900

    Update MainActivity.java

--- Second_Madang/DoitMission03/MainActivity.java
+++ Second_Madang/DoitMission03/MainActivity.java
@@ -1,70 +1,55 @@
 package com.example.mission03;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.res.Resources;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ScrollView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ScrollView scrollView1;
-    ScrollView scrollView2;
-    ImageView imageView1;
-    ImageView imageView2;
-    BitmapDrawable bitmap;
+    ImageView imageView01;
+    ImageView imageView02;
 
-    int index = 0;
-    // up = 0, down = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scrollView1 = findViewById(R.id.scrollView1);
-        scrollView2 = findViewById(R.id.scrollView2);
-        imageView1 = findViewById(R.id.imageView);
-        imageView2 = findViewById(R.id.imageView2);
-        scrollView1.setHorizontalScrollBarEnabled(true);
-        scrollView1.setHorizontalScrollBarEnabled(true);
+        imageView01 = findViewById(R.id.imageView01);
+        imageView02 = findViewById(R.id.imageView02);
 
-        Resources res = getResources();
-        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image01);
-        int bitmapWidth = bitmap.getIntrinsicWidth();
-        int bitmapHeight = bitmap.getIntrinsicHeight();
+        Button button01 = findViewById(R.id.button01);
+        button01.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                moveImageUp();
+            }
+        });
 
-        imageView1.setImageDrawable(bitmap);
-        imageView1.getLayoutParams().width = bitmapWidth;
-        imageView1.getLayoutParams().height = bitmapHeight;
+        Button button02 = findViewById(R.id.button02);
+        button02.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                moveImageDown();
+            }
+        });
 
-        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image01);
-        int bitmap2Width = bitmap.getIntrinsicWidth();
-        int bitmap2Height = bitmap.getIntrinsicHeight();
-
-        imageView2.setImageDrawable(bitmap);
-        imageView2.getLayoutParams().width = bitmap2Width;
-        imageView2.getLayoutParams().height = bitmap2Height;
-
+        moveImageUp();
     }
 
-    public void upbtn(View v){
-        changeUP();
+    private void moveImageDown() {
+        imageView01.setImageResource(0);
+        imageView02.setImageResource(R.drawable.image01);
+
+        imageView01.invalidate();
+        imageView02.invalidate();
     }
 
-    public void downbtn(View v){
-        changeDONW();
-    }
+    private void moveImageUp() {
+        imageView01.setImageResource(R.drawable.image01);
+        imageView02.setImageResource(0);
 
-    private void changeUP() {
-        imageView1.setVisibility(View.VISIBLE);
-        imageView2.setVisibility(View.INVISIBLE);
-    }
-
-    private void changeDONW() {
-        imageView1.setVisibility(View.INVISIBLE);
-        imageView2.setVisibility(View.VISIBLE);
+        imageView01.invalidate();
+        imageView02.invalidate();
     }
 
 }

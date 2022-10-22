4885e4cfea56d0fb673cf3577908007db11c8653
joona <103545438+s-joona@users.noreply.github.com>
2022-10-22 17:08:37 +0900

    Create MainActivity.java

---
+++ Second_Madang/SampleScrollLayout/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+    ScrollView scrollView;
+    ImageView imageView;
+    BitmapDrawable bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scrollView = findViewById(R.id.scrollview);
+        imageView = findViewById(R.id.imageView);
+        scrollView.setHorizontalScrollBarEnabled(true);
+
+        Resources res = getResources();
+        bitmap = (BitmapDrawable)res.getDrawable(R.drawable.oip);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+
+    public void btnClicked(View v){
+        changeImage();
+    }
+
+    private void changeImage() {
+        Resources res = getResources();
+        bitmap = (BitmapDrawable)res.getDrawable(R.drawable.asd);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+}
